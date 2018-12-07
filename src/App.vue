7359840f421dc4ce2e8c5ae372e7f76fb3a5fb24
G-eat <27dhjetori@gmail.com>
2018-12-07 13:20:51 +0100

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app" class="container">
-    <h4>Add user</h4>
+    <h4 v-if='updateUser.email'>Update user</h4>
+    <h4 v-else>Add User</h4>
     <hr style="border: 1px solid rgba(0, 0, 0, 0.5;">
-    <Create @create="newUser" />
+    <Create @create="newUser" @update='updated' v-bind:update=updateUser />
     <!-- <Home msg='123' /> -->
     <hr style="border: 1px solid rgba(0, 0, 0, 0.5;">
     <h4>All users</h4>
     <ol class="collection">
-      <li class="collection-item left-align" v-for='(user , index) in users' :key="index">
-        <span class="switch">
-          <label>
-            Off
-            <input type="checkbox">
-            <span @click='onOff(index)' class="lever"></span>
-            Contacted
-          </label>
+      <li class="collection-item left-align" v-for='(user , index) in users' :key="user.on">
+        <span v-if='user.on'>
+          <span class="switch">
+            <label>
+              <span @click='onOff(index,user.userName)'>Off</span>
+              <input type="checkbox" checked="checked">
+              <span @click='onOff(index,user.userName)' class="lever"></span>
+              <span @click='onOff(index,user.userName)'>Contacted</span>
+            </label>
+          </span>
         </span>
-        <span v-if='user.on' v-bind:style='{ color : color }'>{{ user.userName }} - {{ user.email}}</span>
-        <span v-else>{{ user.userName }} - {{ user.email}}</span>
+        <span v-else>
+          <span class="switch">
+            <label>
+              <span @click='onOff(index,user.userName)'>Off</span>
+              <input type="checkbox">
+              <span @click='onOff(index,user.userName)' class="lever"></span>
+              <span @click='onOff(index,user.userName)'>Contacted</span>
+            </label>
+          </span>
+        </span>
+        <span v-if='user.on' v-bind:style='{ color : color }' @click='update(user ,index)'>{{ user.userName }} - {{ user.email}}</span>
+        <span v-else @click='update(user, index)'>{{ user.userName }} - {{ user.email}}</span>
         <span style="cursor:pointer" class="new badge red darken-4" data-badge-caption="" @click='deleteUser(index)'>Delete</span>
         <span v-bind:class="{ blue: user.gender == 'Male' }" class="new badge red tooltipped" data-badge-caption="age">{{user.age}}</span>
       </li>
@@ -46,6 +59,7 @@ export default {
           on : false
         }
       ],
+      updateUser : '',
       color : 'green'
     }
   },
@@ -54,10 +68,17 @@ export default {
       this.users.push(e);
     },
     onOff(i){
-      this.users[i].on = !this.users[i].on;
+        this.users[i].on = !this.users[i].on;
     },
-    deleteUser(i){
+    deleteUser(i) {
       this.users.splice(i,1);
+    },
+    update(u ,i) {
+      this.updateUser = { userName :u.userName , email :u.email , age :u.age , gender :u.gender , index :i , on :u.on};
+    },
+    updated(e) {
+      this.users.splice(e.index,1);
+      this.users.push(e);
     }
   }
 }
--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,13 +1,14 @@
 <template lang="html">
   <div class="">
     <div v-if="err != ''" class='card-panel red accent-4 white-text'>{{ err }}</div>
-    <form class="col s12" @submit.prevent='newUser'>
+    <div v-if="succ != ''" class='card-panel green accent-4 white-text'>{{ succ }}</div>
+    <form v-if="!update.email" class="col s12" @submit.prevent='newUser'>
       <div class="row">
         <div class="input-field col s6">
-          <input type="text" name="newUser" v-model='input' placeholder="New User">
+          <input  type="text" name="newUser" v-model='input' placeholder="New User">
         </div>
         <div class="input-field col s6">
-          <input type="email" name="email" v-model='email' placeholder="User Email">
+          <input  type="email" name="email" v-model='email' placeholder="User Email">
         </div>
         <div class="input-field col s5">
           <select v-model='gender' class="browser-default">
@@ -24,31 +25,89 @@
         </div>
       </div>
     </form>
+
+    <div v-else>
+      <form class="col s12" @submit.prevent='updateUser'>
+        <div class="row">
+          <div class="input-field col s6">
+            <input type="text" name="updateUser" placeholder="New User" :value="update.userName">
+          </div>
+          <div class="input-field col s6">
+            <input type="email" name="updateemail" placeholder="User Email" :value="update.email">
+          </div>
+          <div class="input-field col s5">
+            <select class="browser-default">
+              <option disabled selected>{{ update.gender }}</option>
+            </select>
+          </div>
+          <div class="input-field col s5">
+            <input type="number" name="updateage" placeholder="Age" :value="update.age">
+          </div>
+          <div class="input-field col s2">
+            <input type="hidden" name="index" :value="update.index">
+            <button type="submit" class='waves-effect waves-light btn' name="updatebutton">Update</button>
+          </div>
+        </div>
+      </form>
+      <div class="input-field col s2">
+        <button @click='cancel' type="submit" class='waves-effect waves-light btn grey' name="cancel">Cancel</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    update: Object
+  },
   data (){
     return {
       input : '',
       email : '',
       age : '',
       gender : 'Gender',
-      err : ''
+      err : '',
+      succ : ''
     }
   },
   methods: {
     newUser (e) {
       if (e.target.newUser.value.trim().length > 0 && this.email != '' && (this.age >= 16 && this.age <= 80) && this.gender != 'Gender') {
         this.$emit('create',{ userName : e.target.newUser.value , email : this.email , age : this.age , gender : this.gender , on : false });
+        this.succ = 'You created a New User !';
         this.input = '';
         this.email = '';
         this.age = '';
         this.gender = 'Gender';
         this.err = '';
       } else {
-        this.err = "You need to compelte the form !";
+          this.err = "Complite Form.Error!";
+      }
+      this.$nextTick(function () {
+          window.setInterval(() => {
+              this.countDown();
+          },5000);
+      });
+    },
+    countDown() {
+      this.err = '';
+      this.succ = '';
+    },
+    updateUser(e) {
+      if (e.target.updateUser.value.trim().length > 0 && e.target.updateemail.value != '' && (e.target.updateage.value >= 16 && e.target.updateage.value <= 80)) {
+        this.$emit('update',{ userName : e.target.updateUser.value , email : e.target.updateemail.value , age : e.target.updateage.value , gender : this.update.gender , index: e.target.index.value , on:this.update.on});
+        this.succ = 'You updated a User !';
+        this.update.email = '';
+        this.$nextTick(function () {
+          window.setInterval(() => {
+            this.countDown();
+          },5000);
+        });
+      } else {
+        this.err = "Complite Form.Error !";
+        this.succ = '';
         this.$nextTick(function () {
             window.setInterval(() => {
                 this.countDown();
@@ -56,8 +115,8 @@ export default {
         });
       }
     },
-    countDown() {
-      this.err = '';
+    cancel() {
+      this.update.email = '';
     }
   }
 }
